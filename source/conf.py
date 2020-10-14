70df776adb7d0edad75eecb48447c0571bc5a6f6
Yasuhiro Ohara <yasu@nttv6.jp>
2020-10-14 09:26:06 +0900

    apply GNU FDL 1.3.

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,15 @@
 # -- Project information -----------------------------------------------------
 
 project = u'Kamuee ユーザガイド'
-copyright = u'2020, '
+#copyright = u'2020, '
+copyright = u'Copyright (c)  2020  NTT Communications Corporation.\
+    Permission is granted to copy, distribute and/or modify this document\
+    under the terms of the GNU Free Documentation License, Version 1.3\
+    or any later version published by the Free Software Foundation;\
+    with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.\
+    A copy of the license is included in the section entitled "GNU\
+    Free Documentation License".'
+
 author = u''
 author_ja = u''
 
@@ -93,7 +101,17 @@ latex_elements = {
 \\renewcommand{\\bibname}{参考文献}
 \\renewcommand{\\contentsname}{目次}
     ''',
-    # 'maketitle': '\\sphinxmaketitle',
+    'maketitle': '''
+\\newcommand\\sphinxbackoftitlepage{
+Copyright \\copyright  2020  NTT Communications Corporation.\\\\
+Permission is granted to copy, distribute and/or modify this document
+under the terms of the GNU Free Documentation License, Version 1.3
+or any later version published by the Free Software Foundation;
+with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+A copy of the license is included in the source file named "COPYING",
+and can be found at \\url{https://www.gnu.org/licenses/fdl-1.3.html}.
+}\\sphinxmaketitle
+    ''',
     'releasename': 'Revision',
     'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
     'tableofcontents': '''\\sphinxtableofcontents\\addcontentsline{toc}{chapter}{目次}''',
